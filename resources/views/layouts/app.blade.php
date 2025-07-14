248ead35e8f2321d77c89a203ac287c2c956fd2d
Alan Cid <alanciddiaz014@gmail.com>
2025-07-13 20:50:59 -0600

    Creando Footer en layout app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,19 @@
         @endif
     </head>
 
-    <body class="bg-neutral-600 h-[1500px]">
+    {{-- h-[1500px] --}}
+    <body class="bg-neutral-600">
+
+        <!-- ===== HEADER: Navegación principal con logo y sticky ===== -->
         <header class="bg-sky-200/75 backdrop-blur-md p-5 mt-5 border border-white mx-101 shadow-xl shadow-white/45 rounded-full z-50 top-0 sticky">
             <div class="flex justify-between items-center px-4">
+
+                 <!-- Logotipo posicionado con absolute a la izquierda -->
                 <h1 class="absolute left-[-300px] top-1/2 -translate-y-1/2 text-3xl">
                     <a class="text-white" href="/"> <span class="font-black">SQUASH</span><span class="font-extralight">PLASH</span></a>
                 </h1>
+
+                 <!-- Menú de navegación principal -->
                 <nav class="flex gap-5 items-center">
                     <a class="text-white uppercase font-black hover:text-cyan-300 text-sm" href="#">Clases</a>
                     <a class="text-white uppercase font-black hover:text-cyan-300 text-sm" href="#">Instalaciones</a>
@@ -23,8 +30,25 @@
                     <a class="text-white uppercase font-black hover:text-cyan-300 text-sm" href="#">Contacto</a>
                     <a class="text-white uppercase font-black hover:text-cyan-300 text-sm" href="#">Nosotros</a>
                 </nav>
+
             </div>
         </header>
-        </div>
+
+        <!-- Contenido Principal -->
+        <main class="max-w-[1800px] px-4 mx-auto mt-10">
+            <h2 class="font-black text-center text-4xl mb-10 text-white">
+                @yield('title')
+            </h2>
+            @yield('contenido')
+        </main>
+
+
+       <footer class="w-full bg-sky-950 text-white p-6 mt-10">
+            <div class="container mx-auto text-center">
+                <p class="text-sm">&copy; {{now()->year}} SQUASHPLASH. Todos los derechos reservados.</p>
+                <p class="text-xs mt-2 text-sky-300">Desarrollado por Alan Cid Díaz</p>
+            </div>
+        </footer>
+
     </body>
 </html>
