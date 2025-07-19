f029791a7627ab782f6e41f4210318f53ff777b7
Alan Cid <alanciddiaz014@gmail.com>
2025-07-19 04:50:26 -0600

    Añadiendo funcionalidad de modo oscuro con JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,3 +11,41 @@ createIcons({
     stroke: 'currentColor'
   }
 });
+
+
+// Dark Mode y Light Mode
+const btnLight = document.querySelector('#light');
+const btnDark = document.querySelector('#dark');
+
+// Definiendo evento click
+btnDark.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.body.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+});
+
+btnLight.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.body.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+});
+
+// Obteniendo LocarlStorage y estableciendo color del sistema
+const localColor = localStorage.getItem('theme');
+const systemColor = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+if (localColor) {
+    if (localColor === 'dark') {
+    document.body.classList.add('dark');
+    } else {
+    document.body.classList.remove('dark');
+    }
+} else {
+    if (systemColor === 'dark') {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+}
+
+
